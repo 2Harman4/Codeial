382492e3394847d1c0f9daf1fb97456dc8918afc
Harmanjit Singh <harmannamrah@gmail.com>
2021-05-05 20:27:53 +0530

    setup ejs and view engine

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const port = 8000;
 //use express router
 app.use('/',require('./routes/index'));
 
+//setting view engine
+app.set('view engine','ejs')
+
+//path of views
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
